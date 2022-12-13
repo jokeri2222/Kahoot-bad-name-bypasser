0a0fdc18f70dde1028e5968b1180b33a7a2eed2d
jokeri2222 <94902709+jokeri2222@users.noreply.github.com>
2022-12-13 18:36:23 +0200

    Update KahootNameBypasser.user.js

--- KahootNameBypasser.user.js
+++ KahootNameBypasser.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       2222jokeri
 // @match        https://kahoot.it/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=kahoot.it
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
